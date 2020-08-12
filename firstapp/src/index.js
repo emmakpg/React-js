07a7fd4b3e6fec2ab8c789f6c779f9f080fc8c0d
Emmanuel Kpogo <emmkpg@gmail.com>
2020-08-12 23:13:07 +0000

    update

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,6 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./comp_todo/App";
 import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    // console.log("you clicked me!");
+
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="todo-list">
+        <h1 style={{ fontSize: "45px" }}>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Click Me</button>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
