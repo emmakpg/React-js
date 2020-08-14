3c3f406c4d7f3a7e465ff5dc21552324e154ff3d
Emmanuel Kpogo <emmkpg@gmail.com>
2020-08-14 23:53:55 +0000

    update

--- firstapp/src/comp_todo/App.js
+++ firstapp/src/comp_todo/App.js
@@ -9,10 +9,19 @@ class App extends React.Component {
     this.state = {
       todos: todosData,
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(id) {
+    console.log("Working", id);
+
+    this.setState({});
+  }
+
   render() {
     const TodoComponent = this.state.todos.map((item) => (
-      <TodoItem key={item.id} item={item} />
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
     ));
 
     return <div className="todo-list">{TodoComponent}</div>;
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,42 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./comp_todo/App";
 import "./App.css";
-
-class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      count: 0,
-    };
-
-    this.increaseNumber = this.increaseNumber.bind(this);
-    this.resetCount = this.resetCount.bind(this);
-  }
-
-  increaseNumber() {
-    // console.log("you clicked me!");
-
-    this.setState((prevState) => {
-      return {
-        count: prevState.count + 1,
-      };
-    });
-  }
-
-  resetCount() {
-    this.setState({ count: 0 });
-  }
-
-  render() {
-    return (
-      <div className="todo-list">
-        <h1 style={{ fontSize: "45px" }}>{this.state.count}</h1>
-        <button onClick={this.increaseNumber}>Increase</button>
-        <button onClick={this.resetCount}>Reset</button>
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(<App />, document.getElementById("root"));
