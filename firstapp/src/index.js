8e83b7a7538416d65be284e4f3d6afed7f2a0afa
Emmanuel Kpogo <emmkpg@gmail.com>
2020-08-16 23:44:35 +0000

    update

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,6 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./comp_todo/App";
-import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 1500);
+  }
+  render() {
+    return <Conditional isLoading={this.state.isLoading} />;
+  }
+}
+
+function Conditional(props) {
+  return (
+    <div>
+      {props.isLoading ? <h3>Loading</h3> : <h3>Finished Loading Content</h3>}
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
