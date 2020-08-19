8e0a55e743e75ba386a36ebe54486dbf30193c94
Emmanuel Kpogo <emmkpg@gmail.com>
2020-08-19 23:47:28 +0000

    update

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,6 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./comp_todo/App";
-import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: false,
+      joke: {},
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch("https://sv443.net/jokeapi/v2/joke/Any")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ joke: data, isLoading: false });
+      });
+  }
+
+  render() {
+    let joke = this.state.joke;
+
+    if (this.state.isLoading) {
+      return <h3>Loading...</h3>;
+    } else {
+      return (
+        <div>
+          <h3>{joke.setup}</h3>
+          <p>{joke.delivery}</p>
+        </div>
+      );
+    }
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
