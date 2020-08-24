3642f839d9521a5f3438b826b145638f4d533252
Emmanuel Kpogo <emmkpg@gmail.com>
2020-08-24 23:59:36 +0000

    update

--- firstapp/src/comp_memegen/Memegenerator.js
+++ firstapp/src/comp_memegen/Memegenerator.js
@@ -10,6 +10,8 @@ class Memegenerator extends React.Component {
       randImage: "http://i.imgflip.com/1bij.jpg",
       allMemes: [],
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -17,22 +19,44 @@ class Memegenerator extends React.Component {
     fetch("https://api.imgflip.com/get_memes")
       .then((response) => response.json())
       .then((res) => {
-        //console.log(res.data.memes);
-
-        this.setState({ allMemes: res.data.memes });
+        const { memes } = res.data;
+        console.log(memes[0]);
+        this.setState({ allMemes: memes });
       });
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
   }
 
   render() {
     return (
       <div>
-        <h3>MemeGenererator</h3>
-        <img
-          style={{ width: 200, height: 150 }}
-          src={this.state.randImage}
-          alt=""
-        ></img>
-        <p>{this.state.allMemes}</p>
+        <form className="meme-form">
+          <input
+            type="text"
+            name="topText"
+            value={this.state.topText}
+            placeholder="Enter top Text"
+            onChange={this.handleChange}
+          ></input>
+
+          <input
+            type="text"
+            name="bottomText"
+            value={this.state.bottomText}
+            placeholder="Enter bottom Text"
+            onChange={this.handleChange}
+          ></input>
+
+          <button>Generate</button>
+        </form>
+
+        <h3>
+          {this.state.topText} {this.state.bottomText}
+        </h3>
       </div>
     );
   }
