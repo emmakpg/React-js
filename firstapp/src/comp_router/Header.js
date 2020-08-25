4aec386911a7c10da17c2cf419097d778100b875
Emmanuel Kpogo <emmkpg@gmail.com>
2020-08-25 23:56:09 +0000

    update

---
+++ firstapp/src/comp_router/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
