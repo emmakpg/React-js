86d919bc2469f364f18300cb65a73aae5c3ecd64
Emmanuel Kpogo <emmkpg@gmail.com>
2020-08-28 23:47:45 +0000

    update

--- firstapp/src/comp_router/Services.js
+++ firstapp/src/comp_router/Services.js
@@ -1,41 +1,31 @@
 import React, { useState, useEffect } from "react";
 
-class Services extends React.Component {
-  /*   const fetchMeal = async () => {
-    const data = await fetch("https://sv443.net/jokeapi/v2/joke/Any");
-    const items = await data.json();
-    console.log(items); } */
+function Services() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-  constructor() {
-    super();
+  const [items, setItems] = useState([]);
 
-    this.state = {
-      setup: "",
-      delivery: "",
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://sv443.net/jokeapi/v2/joke/Any")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        this.setState({
-          setup: data.setup,
-          delivery: data.delivery,
-        });
-      });
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>Services Page</h3>
-        <p>{this.state.setup}</p>
-        <p>{this.state.delivery}</p>
-      </div>
+  const fetchItems = async () => {
+    const data = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/search.php?f=c"
     );
-  }
+    const items = await data.json();
+    //console.log(items.meals);
+    setItems(items.meals);
+  };
+
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h3>Services Page</h3>
+      {items.map((item) => {
+        return <p key={item.idMeal}>{item.strMeal}</p>;
+      })}
+    </div>
+  );
 }
 
 export default Services;
